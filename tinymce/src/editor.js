86518f4749c09e2a2eabb091807561d7725a3d69
Alex Lockhart <alexlockahrt7@gmail.com>
2020-07-29 07:42:56 -0400

    Added save feature to tinymce

--- tinymce/src/editor.js
+++ tinymce/src/editor.js
@@ -1,3 +1,27 @@
 tinymce.init({
-    selector: 'textarea'
+    selector: '.editor',
+    menubar: 'edit view format',
+    autosave_interval: "30s",
 });
+
+const saveBtn = document.querySelector(".save");
+const saved = document.querySelector(".saved");
+
+setInterval(() => {
+    save();
+}, 5000);
+
+saveBtn.addEventListener('click', () => save() && saveAnimation());
+
+function save() {
+    console.log(tinymce.activeEditor.getContent());
+
+    return true;
+}
+
+function saveAnimation() {
+    saveBtn.innerHTML = `&check; Saved`;
+    setTimeout(() => {
+        saveBtn.innerHTML = `Save`;
+    }, 1000)
+}
